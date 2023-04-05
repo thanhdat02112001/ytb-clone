3bde3856f46f97efc8c52efa519f37dd6031c74a
thanhdat02112001 <thanhdat02112001pt@gmail.com>
2023-04-05 15:01:48 +0700

    remove inertia

--- src/app/Models/User.php
+++ src/app/Models/User.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 // use Illuminate\Contracts\Auth\MustVerifyEmail;
-use Illuminate\Support\Str;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
@@ -12,8 +11,6 @@ use Laravel\Sanctum\HasApiTokens;
 class User extends Authenticatable
 {
     use HasApiTokens, HasFactory, Notifiable;
-
-    public $incrementing = false;
 
     /**
      * The attributes that are mass assignable.
@@ -44,16 +41,4 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
-
-    protected static function boot()
-    {
-        parent::boot();
-        
-        static::creating(function ($model) {
-            $model->id = Str::uuid();
-        });
-
-    }
-
-
 }
--- src/resources/views/app.blade.php
+++ src/resources/views/app.blade.php
@@ -1,21 +1,15 @@
 <!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>YTB</title>
 
-        <title inertia>{{ config('app.name', 'Laravel') }}</title>
+	@vite('resources/css/app.css')
+</head>
+<body>
+	<div id="app"></div>
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
-
-        <!-- Scripts -->
-        @routes
-        @vite(['resources/js/app.js', "resources/js/Pages/{$page['component']}.vue"])
-        @inertiaHead
-    </head>
-    <body class="font-sans antialiased">
-        @inertia
-    </body>
+	@vite('resources/js/app.js')
+</body>
 </html>
